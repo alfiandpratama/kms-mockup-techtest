a7797418326e12cbbdde4a8139a417057b63df02
alfiandpratama <pratama.d.alfian@gmail.com>
2020-10-18 22:00:44 +0700

    Updates

---
+++ precache-manifest.ec82985e42933cc18c92d1c3d9609f4e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb494c502bf3a4caad99ff00df317a8c",
+    "url": "/alfiandpratama/kms-mockup-techtest/index.html"
+  },
+  {
+    "revision": "dd2b5629b77ee5d5fede",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/css/3.c31d5777.chunk.css"
+  },
+  {
+    "revision": "2803b179089cbee03c81",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/css/4.b26df3bb.chunk.css"
+  },
+  {
+    "revision": "9671d7ec6adf04393b57",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/css/main.1152f335.chunk.css"
+  },
+  {
+    "revision": "1e0a07d3b693b1687c8c",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/2.0c30cbfe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/2.0c30cbfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2b5629b77ee5d5fede",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/3.c8417a77.chunk.js"
+  },
+  {
+    "revision": "ecef787518253a5c6da06dfb3c82bca1",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/3.c8417a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2803b179089cbee03c81",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/4.e9381e9c.chunk.js"
+  },
+  {
+    "revision": "86060194d9c86e8895bcfebf7c789f50",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/4.e9381e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b253e9c920fcb42e829",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/5.289aaced.chunk.js"
+  },
+  {
+    "revision": "43a0c646e2f0b545c2bc",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/6.80cb8a1e.chunk.js"
+  },
+  {
+    "revision": "7821a622cc556d204cdb",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/7.4ab39c45.chunk.js"
+  },
+  {
+    "revision": "d0741b3ff789813df81b",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/8.0b859e2a.chunk.js"
+  },
+  {
+    "revision": "90b9023acc281dcb4212",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/9.b8b8cf3c.chunk.js"
+  },
+  {
+    "revision": "9671d7ec6adf04393b57",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/main.fe1b3db6.chunk.js"
+  },
+  {
+    "revision": "173886f458bfcbf92550",
+    "url": "/alfiandpratama/kms-mockup-techtest/static/js/runtime-main.4101da18.js"
+  }
+]);
---
+++ static/js/9.b8b8cf3c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkms-mockup"]=this["webpackJsonpkms-mockup"]||[]).push([[9],{165:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a);t.default=function(){return r.a.createElement(r.a.Fragment,null,r.a.createElement("h1",null,"Generate Reports"))}}}]);
+//# sourceMappingURL=9.b8b8cf3c.chunk.js.map
