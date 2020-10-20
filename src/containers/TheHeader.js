c2f9af97cb2506a7b9644ab8b92b8edabf794d4c
alfiandpratama <pratama.d.alfian@gmail.com>
2020-10-20 10:56:02 +0700

    fixed static subheader

--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -17,32 +17,51 @@ import { TheHeaderDropdown } from './index'
 import routes from '../routes'
 
 const TheHeader = () => {
+  // get access to history route instance
   const history = useHistory()
 
+  // get dispatch reference and extract data from redux store state
   const dispatch = useDispatch()
   const sidebarShow = useSelector((state) => state.sidebarShow)
 
+  // show toggle sidebar mobile viewport event
   const toggleSidebarMobile = () => {
     const val = [false, 'responsive'].includes(sidebarShow)
       ? true
       : 'responsive'
     dispatch({ type: 'set', sidebarShow: val })
   }
+
+  // static subheader show
+  const isSubheader =
+    history.location.pathname === '/dashboard' ||
+    history.location.pathname === '/reports'
+
   return (
     <CHeader withSubheader>
+      {/* START: Toggler Sidebar Menu */}
       <CToggler
         inHeader
         className='ml-md-3 d-lg-none'
         onClick={toggleSidebarMobile}
       />
+      {/* END: Toggler Sidebar Menu */}
+
+      {/* START: Breadcrumb router */}
       <CBreadcrumbRouter
         className='border-0 c-subheader-nav m-0 px-0 px-md-4 mr-auto'
         routes={routes}
       />
+      {/* END: Breadcrumb router */}
+
+      {/* START: Header Navigation */}
       <CHeaderNav className='px-4'>
         <TheHeaderDropdown />
       </CHeaderNav>
-      {history.location.pathname === '/dashboard' && (
+      {/* END: Header Navigation */}
+
+      {/* START: Subheader */}
+      {isSubheader && (
         <CSubheader className='px-4 justify-content-between'>
           <CHeaderNav className='mr-auto'>
             <CHeaderNavItem>
@@ -56,6 +75,7 @@ const TheHeader = () => {
           </CHeaderNav>
         </CSubheader>
       )}
+      {/* END: Subheader */}
     </CHeader>
   )
 }
